6491ac14657835b7acde96a876abb2247382c146
Kgothatso <kgothatso.seboya@accenture.com>
2024-03-08 16:09:57 +0200

    initial commit

--- src/main/java/co/za/enkstudio/config/CacheConfiguration.java
+++ src/main/java/co/za/enkstudio/config/CacheConfiguration.java
@@ -48,6 +48,18 @@ public class CacheConfiguration {
             createCache(cm, co.za.enkstudio.domain.User.class.getName());
             createCache(cm, co.za.enkstudio.domain.Authority.class.getName());
             createCache(cm, co.za.enkstudio.domain.User.class.getName() + ".authorities");
+            createCache(cm, co.za.enkstudio.domain.Region.class.getName());
+            createCache(cm, co.za.enkstudio.domain.Country.class.getName());
+            createCache(cm, co.za.enkstudio.domain.Location.class.getName());
+            createCache(cm, co.za.enkstudio.domain.Department.class.getName());
+            createCache(cm, co.za.enkstudio.domain.Department.class.getName() + ".employees");
+            createCache(cm, co.za.enkstudio.domain.Task.class.getName());
+            createCache(cm, co.za.enkstudio.domain.Task.class.getName() + ".jobs");
+            createCache(cm, co.za.enkstudio.domain.Employee.class.getName());
+            createCache(cm, co.za.enkstudio.domain.Employee.class.getName() + ".jobs");
+            createCache(cm, co.za.enkstudio.domain.Job.class.getName());
+            createCache(cm, co.za.enkstudio.domain.Job.class.getName() + ".tasks");
+            createCache(cm, co.za.enkstudio.domain.JobHistory.class.getName());
             // jhipster-needle-ehcache-add-entry
         };
     }
--- src/main/webapp/app/entities/entity.routes.ts
+++ src/main/webapp/app/entities/entity.routes.ts
@@ -1,6 +1,46 @@
 import { Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: 'region',
+    data: { pageTitle: 'jhipstermonov001App.region.home.title' },
+    loadChildren: () => import('./region/region.routes'),
+  },
+  {
+    path: 'country',
+    data: { pageTitle: 'jhipstermonov001App.country.home.title' },
+    loadChildren: () => import('./country/country.routes'),
+  },
+  {
+    path: 'location',
+    data: { pageTitle: 'jhipstermonov001App.location.home.title' },
+    loadChildren: () => import('./location/location.routes'),
+  },
+  {
+    path: 'department',
+    data: { pageTitle: 'jhipstermonov001App.department.home.title' },
+    loadChildren: () => import('./department/department.routes'),
+  },
+  {
+    path: 'task',
+    data: { pageTitle: 'jhipstermonov001App.task.home.title' },
+    loadChildren: () => import('./task/task.routes'),
+  },
+  {
+    path: 'employee',
+    data: { pageTitle: 'jhipstermonov001App.employee.home.title' },
+    loadChildren: () => import('./employee/employee.routes'),
+  },
+  {
+    path: 'job',
+    data: { pageTitle: 'jhipstermonov001App.job.home.title' },
+    loadChildren: () => import('./job/job.routes'),
+  },
+  {
+    path: 'job-history',
+    data: { pageTitle: 'jhipstermonov001App.jobHistory.home.title' },
+    loadChildren: () => import('./job-history/job-history.routes'),
+  },
   /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
 ];
 
